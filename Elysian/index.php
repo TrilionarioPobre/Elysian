838d82949768a20ca4eb540440fba2081a9add8d
TrilionarioPobre <112767602+TrilionarioPobre@users.noreply.github.com>
2022-09-06 12:12:35 -0300

    Update

--- Elysian/index.php
+++ Elysian/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,82 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Elysian</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/stylelogin.css">
   <link rel="stylesheet" href="img/img.css">
-<style>
-    body {
-    background: #295872;
-    font-family: 'Open Sans', sans-serif;
-    padding: 10px;
-  }
-    body {font-family: 'Open Sans', sans-serif;}
-    form {border:3px solid black;}
-
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  display: inline-block;
-  border: 1px solid #357599;
-  box-sizing: border-box;
-}
-
-button {
-  background-color: #357599;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-
-.imgcontainer {
-  text-align: center;
-  position: center;
-}
-
-
-.login {
-  padding: 12px 20px;
-}
-
-img.avatar {
-  width: 200px;
-  border-radius: 300px;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding: 10px 18px;
-  width: auto;
-}
-
-.modal {
-	display: none;
-	position: fixed;
-	z-index: 1;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	overflow: auto;
-	padding-top: 60px;
-}
-
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-</style>
 </head>
 <body>
     	<img style="width: 100%; height: 400px;" src="img/banner.jpeg">
@@ -94,17 +19,14 @@ span.psw {
             <li class="active" style="float: right;"><a href="login.html">Login</a></li>
         </ul>
     </nav>
-
 <form action="" method="post" style="background-color: white;">
 	
 	<div class="login">
 		<h1>Iniciar Sessão</h1>
 	</div>
-
 	<div class="imgcontainer">
 		<img src="img/avatar.png" alt="Avatar" class="avatar">
 	</div>
-
 	<div class="container">
 		<label for="email"><b>Conta Elysian (E-mail)</b></label>
 		<input type="text" placeholder="Nome de Usuário"name="email" required>
